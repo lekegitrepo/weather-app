9f40c918621e9aa275cd47ae655a7ef9c282f995
lekegitrepo <lekedevrepo@gmail.com>
2020-02-19 15:09:53 -0600

    create fetchWeatherApi function

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
 import './scss/app.scss';
+
+const fetchWeatherApi = (location) => {
+  const api = fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=3200d53ac65b442eb5f439f5613ee06c`, {mode: 'cors'})
+  .then((res) => {
+    return res.json();
+  })
+  .then((res) => {
+    console.log(res);
+  }).catch((err) => {
+    console.log(err);
+  })
+}
+
+fetchWeatherApi('London');
+fetchWeatherApi('Paris');
