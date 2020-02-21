e8fb54a82d162b1cd210c2c5e279828acb8534a3
lekegitrepo <lekedevrepo@gmail.com>
2020-02-21 11:11:00 -0600

    update the celsius and fahrenheit logic

--- src/app.js
+++ src/app.js
@@ -7,21 +7,7 @@ const btnCelsius = document.getElementById('celsius');
 const btnFahren = document.getElementById('fahrenheit');
 const loader = document.getElementById('loader-container');
 
-/*const fetchWeatherApi = (location) => {
-  const api = fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=3200d53ac65b442eb5f439f5613ee06c`, {mode: 'cors'})
-  .then((res) => {
-    return res.json();
-  })
-  .then((res) => {
-    console.log(res);
-    getJSON(res);
-    loader.style.display = 'none';
-    return res;
-  }).catch((err) => {
-    loader.style.display = 'none';
-    console.log(err);
-  })
-}*/
+let dataHub;
 
 async function fetchWeatherApi (location) {
   try{
@@ -32,21 +18,47 @@ async function fetchWeatherApi (location) {
   }catch (err) {
     console.log('error: unable to fetch weather info');
   }
-  //return resp;
 }
 
 async function displayWeatherInfo() {
-      const info = await fetchWeatherApi(input.value).catch((err) => {console.log(err)});
-      console.log(info);
-      /*weatherInfo.innerHTML = `<div>Weather Info</div>
-        <p>City: ${jsonData.name}</p>
-        <p>Country: ${jsonData.sys.country}</p>
-        <p>Temperature: ${jsonData.main.temp}</p>
-        <p>Weather: ${jsonData.weather[0].main}</p>
-        <p></p>`*/
-    }
+  const info = await fetchWeatherApi(input.value).catch((err) => {console.log(err)});
+  console.log(info);
+  dataHub = getJSON(info);
+  getJSONOld(info)
+}
 
 const getJSON = (jsonData) => {
+
+  const location = () => {
+    return {city: jsonData.name, country: jsonData.sys.country};
+  }
+
+  const temp = () => {
+    return {mainTemp: jsonData.main.temp, humidity: jsonData.main.humidity, pressure: jsonData.main.pressure};
+  }
+
+  const weather = () => {
+    return {weather: jsonData.weather[0].main, description: jsonData.weather[0].description};
+  }
+
+  const wind = () => {
+    return {speed: jsonData.wind.speed, deg: jsonData.wind.deg};
+  }
+
+  const coordinate = () => {
+    return {lon: jsonData.coord.lon, lat: jsonData.coord.lat};
+  }
+
+  return {
+    location,
+    temp,
+    weather,
+    wind,
+    coordinate
+  }
+};
+
+const getJSONOld = (jsonData) => {
   console.log(jsonData.name);
   weatherInfo.innerHTML = `<div>Weather Info</div>
     <div class="container">
@@ -71,6 +83,7 @@ const getJSON = (jsonData) => {
 
 btn.addEventListener('click', async () => {
   await displayWeatherInfo();
+  console.log(dataHub.location().city);
   //fetchWeatherApi(input.value);
   //loader.style.display = 'block';
   //fetchWeatherApi('London');
@@ -79,17 +92,17 @@ btn.addEventListener('click', async () => {
 
 btnCelsius.addEventListener('click', () => {
   const temp = document.getElementById('temp');
-  const cels = Math.round(parseFloat(temp.innerText,  10) - 273.15);
-  console.log(temp.innerText)
+  const cels = Math.round(dataHub.temp().mainTemp - 273.15);
+  console.log(dataHub.temp().mainTemp)
   temp.innerHTML = cels + '°C';
   console.log(cels)
 })
 
 btnFahren.addEventListener('click', () => {
   const temp = document.getElementById('temp');
-  const cels = Math.round(parseFloat(temp.innerText,  10) - 273.15);
+  const cels = Math.round(dataHub.temp().mainTemp - 273.15);
   const faren = Math.round(cels * (9 / 5) + 32);
-  console.log(temp.innerText)
+  console.log(dataHub.temp().mainTemp)
   temp.innerHTML = faren + '°F';
-  console.log(cels)
+  console.log(faren)
 })
